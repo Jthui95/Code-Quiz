eb1434c29048d575844715d3a1fb677fd8097081
Jthui95 <60634411+Jthui95@users.noreply.github.com>
2020-07-08 16:21:18 -0700

    added a timer to the upper left and added the ability to keep track of score and subtract time based on wrong answers.

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
+//start button
 let startBtn = document.querySelector('#start')
-let score = 0;
-// let count = ;
-// let hs = ;
-// let count = localStorage.getItem("count");
-// counter.textContent = count;
+//score tracker
+let userScore = 0;
+//time related variables
+let timeLeft = document.querySelector('#left');
+let secondsLeft = 75;
+// relates to the formating of the quiz
 let questions = document.querySelector("#questions");
 let mainEl = document.querySelector('#main');
 let timerEl = document.querySelector("#countdown");
 let bodyEl = document.createElement("div");
 
 
-
-// function quizSubmit(event){
-//     event.preventDefault();
-//     console.log('Event', event);
-    
-//     let response = `Correct! ${asd}`;
-//     }
-// click button to start quiz countdown 
+//starts the quiz
 startBtn.addEventListener('click', quiz);
-    
-// function prepareQuiz() {
-//     let timeLeft = 5;
-//     let timeInterval = setInterval(countDown, 1000);
-//     function countDown(){
-//         timerEl.textContent = `${timeLeft} seconds till quiz starts`;
-//         timeLeft--;
 
-//         if(timeLeft === 0){
-//             timerEl.textContent = "";
-//             clearInterval(timeInterval);
-//             quiz();
-//         }
-//     }   
-
-
-// Question 1
-function quiz(){
+// Starts timer and quiz
+function quiz() {
+    var timeInterval = setInterval(function(){
+        secondsLeft--;
+timeLeft.textContent = `${secondsLeft} seconds till game ends.`
+    if(secondsLeft === 0){
+    clearInterval(timeInterval)
+    alert("Game Over");
+    }
+}, 1000 );
+qOne();
+//creates the question and answer
+    function qOne(){
     let h = document.createElement("h2");
     h.textContent = "What is the main characters name in Pokemon?";
     questions.appendChild(h);
-    
+
     let answer = ["Ash", "Brock", "Jake"];
    
     //creates the answer
     for(let i = 0; i < answer.length; i++ ){
         let answers = answer[i];
-
         let li = document.createElement('li');
-        // li.textContent = answers;
         li.setAttribute('data-index',i);
 
         let button = document.createElement('button');
@@ -57,9 +46,22 @@ function quiz(){
         li.appendChild(button);
         questions.append(li);
     }
+    questions.addEventListener("click", function(){
+        if(answer === "Ash"){
+            userScore++;
+            alert("That's Correct!");
+            setScoreText();
+        }else(answer === "Brock" || answer === "Jake")
+            secondsLeft--;
+            alert("That's Incorrect!")
+    })
+
+            
     
-}
 // }
+// }
+// }
+
 // if (element.matches('button')=== true){
 //     let element = event.target
 
@@ -81,8 +83,8 @@ function quiz(){
 //             bodyEl.textContent = array[currentQuestion];
 //             currentQuestion++;
 //         }
-//     }
-// }
+    }
+ }
 
 // function quizSubmit(event){
 // event.preventDefault();
@@ -94,3 +96,8 @@ function quiz(){
 // each functio needs to ask their own question and have their own answer
 // each function needs to have a ul and li to make options 
 //input = something you collect info on
+
+// make timer for the code on upper left
+// add the ability to keep track of score for correct answer refer back to rock paper scizzors activity?
+// keep track of score and stuff with computer storage high scores 
+
